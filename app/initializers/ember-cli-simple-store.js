d16e84e97303223dace0add7235a76f3d09ea4f9
toran billups <toranb@gmail.com>
2015-01-04 19:54:48 -0600

    [BUG]: updated initializer name to be store/removed ember-data

--- app/initializers/ember-cli-simple-store.js
+++ app/initializers/ember-cli-simple-store.js
@@ -1,6 +1,6 @@
 import initializer from 'ember-cli-simple-store/initializers/ember-cli-simple-store';
 
 export default {
-    name: 'ember-cli-simple-store',
+    name: 'store',
     initialize: initializer
 };
