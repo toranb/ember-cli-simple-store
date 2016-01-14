edf2d41b60ab2959224146dee4169e9805189a7a
Jason Mitchell <jasonmit@nestlabs.com>
2016-01-14 14:00:09 -0800

    Conditionally disable initializer

--- addon/initializers/ember-cli-simple-store.js
+++
@@ -1,8 +0,0 @@
-import Store from '../store';
-
-export default function() {
-    var application = arguments[1] || arguments[0];
-    application.register('store:main', Store);
-    application.inject('controller', 'store', 'store:main');
-    application.inject('route', 'store', 'store:main');
-}
