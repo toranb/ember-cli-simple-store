3dd29d5b01c864a98e0e22da2524bb974aaf2cde
toran billups <toranb@gmail.com>
2016-03-08 20:59:19 -0600

    [DEPENDENCY]: updated to ember-cli 2.4.2

--- tests/helpers/registration.js
+++ tests/helpers/registration.js
@@ -1,7 +1,7 @@
 import SimpleStore from "ember-cli-simple-store/store";
 
 export default function(owner, keys) {
-    var factory = window.require("ember/resolver")["default"];
+    var factory = window.require("ember-resolver")["default"];
     var resolver = factory.create({ namespace: { modulePrefix: "dummy" }});
 
     owner.register("service:simple-store", SimpleStore);
