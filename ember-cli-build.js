48846b3e246ff91268aca273b7a40c8dc7481a36
snewcomer <snewcomer24@gmail.com>
2017-09-23 18:25:03 -0700

    update with native-dom-event-dispatcher

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,10 +3,15 @@
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {
-    vendorFiles: { 'jquery.js': null }
-  });
+module.exports = function(defaults) {  
+  let project = defaults.project;
+  let options = {};
+
+  if (project.findAddonByName('ember-native-dom-event-dispatcher') && process.env.DEPLOY_TARGET === undefined) {
+    options.vendorFiles = { 'jquery.js': null };
+  }
+
+  let app = new EmberAddon(defaults, options);
 
   /*
     This build file specifies the options for the dummy test app of this
