174568e8d99d04d3581df76d5d9805d71e23ab9f
snewcomer <snewcomer24@gmail.com>
2017-09-24 18:02:51 -0700

    run aware test in test-support

--- tests/unit/arrays-test.js
+++ tests/unit/arrays-test.js
@@ -3,31 +3,34 @@ import { getOwner } from '@ember/application';
 import { moduleFor } from "ember-qunit";
 import { test } from "dummy/tests/helpers/qunit";
 
-let store, Thing, Stuff;
+let Thing, Stuff;
 
 moduleFor("service:simple-store", "arrays unit tests", {
     beforeEach: function () {
         const owner = getOwner(this);
-        store = this.subject();
+        this.store = this.subject();
         Stuff = EmberObject.extend();
 
         Thing = EmberObject.extend({
             observationCount: 0,
             name: "",
-            stuff: store.find("stuff"),
+            stuff: this.store.find("stuff"),
             observeStuff: observer("stuff.[]", function () {
                 let currentObservations = this.get("observationCount");
                 this.set("observationCount", currentObservations + 1);
             })
         });
         owner.register("model:stuff", Stuff);
+    },
+    afterEach: function() {
+        delete this.store;
     }
 });
 
 test("observers are only notified once regardless of the number of models added to the store", function (assert) {
     let thing1 = Thing.create({name: "thing1"});
 
-    store.pushArray("stuff", [{ id: "1", name: "stuff1"}, { id: "2", name: "stuff2"}]);
+    this.store.pushArray("stuff", [{ id: "1", name: "stuff1"}, { id: "2", name: "stuff2"}]);
 
     assert.equal(thing1.get("observationCount"), 1);
 });
